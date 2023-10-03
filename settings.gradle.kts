6c4028728b3d92f45c437a0d904653b7bc75167b
Thiago Contelli <thiagocontellid@gmail.com>
2023-10-02 21:18:36 -0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TabNews Mobile"
+include(":app")
+ 
