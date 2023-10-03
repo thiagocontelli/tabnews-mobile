e985a2545eb0f60aa1c0beafcb3e6bc0c58c5cdf
Thiago Contelli <thiagocontellid@gmail.com>
2023-10-02 21:25:41 -0300

    start using view binding

--- app/src/main/java/com/thiagocontelli/tabnewsmobile/MainActivity.kt
+++ app/src/main/java/com/thiagocontelli/tabnewsmobile/MainActivity.kt
@@ -2,10 +2,12 @@ package com.thiagocontelli.tabnewsmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thiagocontelli.tabnewsmobile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
